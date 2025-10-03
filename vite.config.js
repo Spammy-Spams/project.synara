5f60a6508feb99bc66eb15e15cb7b793d3b7fd78
Spammy-Spams <thespammyspamsspammy@gmail.com>
2025-10-03 06:03:35 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+javascript
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Replace with your actual GitHub repo name
+export default defineConfig({
+  plugins: [react()],
+  base: "/my-synara-website/", 
+});
