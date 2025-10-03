a8ef4126fb912ac075402a1ff0f1e0fffca2e030
Spammy-Spams <thespammyspamsspammy@gmail.com>
2025-10-03 05:50:00 -0400

    Create ButterflySVG.js

---
+++ src/components/ButterflySVG.js
@@ -0,0 +1,10 @@
+javascript
+import React from "react";
+
+export default function ButterflySVG({ className }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className={className}>
+      <path d="M12 2c-1.5 0-3 .8-3.8 2.2C7.2 6 6 8 4 8c-1.7 0-3 1.3-3 3s1.3 3 3 3c2 0 3.2 2 4.2 3.8C9 21.2 10.5 22 12 22s3-.8 3.8-2.2c1-1.8 2.2-3.8 4.2-3.8 1.7 0 3-1.3 3-3s-1.3-3-3-3c-2 0-3.2-2-4.2-3.8C15 2.8 13.5 2 12 2z" />
+    </svg>
+  );
+}
